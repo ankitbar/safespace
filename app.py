463506f8575957423aecf05284fcf2ae23c6b2ba
Ankit Baraskar <ankit.baraskar1@gmail.com>
2023-06-10 00:08:08 +0530

    Added some authentication request code

--- app.py
+++ app.py
@@ -14,12 +14,6 @@ app.secret_key = 'your_secret_key'
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf', 'mp4'}
 
-# Example hardcoded user credentials - PROBABLY REDUNDANT
-USERS = [
-    {'username': 'user1', 'password': generate_password_hash('password1')},
-    {'username': 'user2', 'password': generate_password_hash('password2')}
-]
-
 # Create a SQLite database connection
 conn = sqlite3.connect('users.db',check_same_thread=False)
 cursor = conn.cursor()
@@ -46,38 +40,98 @@ db = SQLAlchemy(app)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# THIS IS PROBABLY REDUNDANT
-def get_user(username):
-    for user in USERS:
-        if user['username'] == username:
-            return user
-    return None
+# Database models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    files = db.relationship('File', backref='user', lazy=True)
 
-def verify_user(username, password):
-    user = get_user(username)
-    if user and check_password_hash(user['password'], password):
-        return user
-    return None
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-# END OF POSSIBLY REDUNDANT CODE
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    shared_with = db.relationship('User', secondary='shared_files', lazy='subquery',
+                                  backref=db.backref('shared_files', lazy=True))
+
+    def __init__(self, filename, user_id):
+        self.filename = filename
+        self.user_id = user_id
+
+shared_files = db.Table('shared_files',
+    db.Column('file_id', db.Integer, db.ForeignKey('file.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+db.create_all()
+
+# Helper functions
+def send_email(receiver_email, subject, message):
+    # Code to send email request to receiver_email
+    pass
+
+def authenticate_file_access_request(requester, file_owner, file_name):
+    receiver_email = file_owner.username  # Assume email address is the same as username
+    subject = f'File Access Request: {file_name}'
+    message = f'User {requester.username} is requesting access to your file: {file_name}. ' \
+              f'Please approve or decline the request.'
+    send_email(receiver_email, subject, message)
+
+def get_user_files(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return user.files
+    return []
+
 
 @app.route('/')
 def index():
     print(os.listdir('/'))
     return render_template('index.html')
 
-@app.route('/upload/<username>', methods=['GET', 'POST'])
+@app.route('/upload/<username>', methods=['POST'])
 def upload(username):
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = file.filename
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            flash(f'File "{filename}" uploaded successfully.')
-        else:
-            flash('Invalid file type. Please upload an image, video, or PDF file.')
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        # Generate a unique filename to avoid collisions
+        filename = f'{username}_{file.filename}'
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+
+        # Upload the file to S3
+        s3.upload_fileobj(file, S3_BUCKET, file_path)
+
+        # Create the file record in the database
+        user = User.query.filter_by(username=username).first()
+        file_record = File(filename=filename, user_id=user.id)
+        db.session.add(file_record)
+        db.session.commit()
+
+        flash(f'File "{filename}" uploaded successfully.')
+    else:
+        flash('Invalid file type. Please upload an image, video, or PDF file.')
 
     return redirect(url_for('landing', username=username))
+
+@app.route('/request_access/<username>/<file_id>', methods=['GET'])
+def request_access(username, file_id):
+    requester = User.query.filter_by(username=username).first()
+    file = File.query.filter_by(id=file_id).first()
+    if not requester or not file:
+        flash('Invalid request.')
+        return redirect(url_for('index'))
+
+    if requester in file.shared_with:
+        flash('Access already granted.')
+    else:
+        authenticate_file_access_request(requester, file.user, file.filename)
+        flash('Access request sent to the file owner.')
+
+    return redirect(url_for('landing', username=username))
+
 
 @app.route('/files')
 def files():
@@ -93,38 +147,30 @@ def download(filename):
         flash('You do not have permission to download this file.')
         return redirect(url_for('files'))
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    username = request.form['username']
+    password = request.form['password']
 
-        # Hash the password before storing it in the database
-        hashed_password = generate_password_hash(password)
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        flash('Username already exists. Please choose a different username.')
+        return redirect(url_for('index'))
 
-        # Insert the user information into the database
-        try:
-            cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed_password))
-            conn.commit()
-            flash('Registration successful. Please log in.')
-            return redirect(url_for('index'))
-        except sqlite3.IntegrityError:
-            flash('Username already exists. Please choose a different username.')
+    user = User(username=username, password=generate_password_hash(password))
+    db.session.add(user)
+    db.session.commit()
 
-    return render_template('register.html')
+    flash('Registration successful. Please log in.')
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
 
-    # Retrieve the user from the database
-    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
-    user = cursor.fetchone()
-
-    if user and check_password_hash(user[2], password):
-        flash('Login successful.')
-        # Additional logic after successful login
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
         return redirect(url_for('landing', username=username))
     else:
         flash('Invalid username or password.')
@@ -137,9 +183,8 @@ def landing(username):
         # Logic to create a new folder using folder_name
         flash(f'Folder "{folder_name}" created successfully.')
 
-    # Logic to retrieve the user's files and folders
     files = get_user_files(username)
-    folders = get_user_folders(username)
+    folders = []  # Placeholder for folder retrieval logic
 
     return render_template('landing.html', username=username, files=files, folders=folders)
 
